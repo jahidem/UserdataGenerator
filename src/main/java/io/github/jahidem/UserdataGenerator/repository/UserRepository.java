2d2d4aace27763b8dfed52267259d15e906f8aae
Jahid Hossain <jahid.hossain.e99@gmail.com>
2024-07-26 21:04:54 +0600

    fix: don't persist users data

--- src/main/java/io/github/jahidem/UserdataGenerator/repository/UserRepository.java
+++ src/main/java/io/github/jahidem/UserdataGenerator/repository/UserRepository.java
@@ -17,8 +17,6 @@ public class UserRepository {
   private Faker faker;
   private long error;
 
-  private final List<User> users = new ArrayList<>();
-
   public UserRepository() {
     this.initialize("en", "US", 0, 0);
   }
@@ -27,8 +25,6 @@ public class UserRepository {
   public void initialize(String language, String country, long seed, double currError) {
     this.locale = new Locale(language, country);
     this.seed = seed;
-    this.removeAll();
-    this.initializeUser(20, 0);
 
     this.error = (long) Math.floor(currError);
     // the fraction part of error act asprobability of occuring one more error for
@@ -36,17 +32,15 @@ public class UserRepository {
     this.error += this.getRandomDouble(1) < currError - Math.floor(currError) ? 1 : 0;
   }
 
-  private void initializeUser(int number, int page) {
+  private List<User> initializeUser(int number, int page) {
     this.random = new Random(this.seed + page);
     this.faker = new Faker(locale, random);
-    for (int u = 0; u < number; u++)
+
+    List<User> users = new ArrayList<>();
+    for (int u = 0; u < 20; u++)
       users.addLast(
           this.fakeUser());
-  }
-
-  private void removeAll() {
-    while (!users.isEmpty())
-      users.removeLast();
+    return users;
   }
 
   private User fakeUser() {
@@ -60,12 +54,11 @@ public class UserRepository {
   }
 
   public List<User> getUserList() {
-    return users;
+    return initializeUser(20, 0);
   }
 
   public List<User> getUserPage(int page) {
-    initializeUser(10, page);
-    return users.subList(20 + (page-1) * 10, 20 + (page-1) * 10 + 10); // 10 users
+    return initializeUser(10, page);
   }
 
   public int getRandom(int upperBound) {
